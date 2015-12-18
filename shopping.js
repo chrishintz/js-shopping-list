aa17480785f470801700bd40549f352057870a3c
Chris Hintz <chrishintz42@gmail.com>
2015-12-18 00:08:05 -0800

    Refactor div/list adds, add delete and gray functionality

--- shopping.js
+++ shopping.js
@@ -1,52 +1,62 @@
-window.onload = function() {
-  var addToList = document.getElementById("addToList");
-  var list      = document.getElementById("list");
-  var li;
+window.onload = function () {
+  // start by listing variables for future list, submit button and input box.
+  // Variable equal to document.getElementById("idname from html")
+  var list    = document.getElementById("list");
+  var submit  = document.getElementById("submit");
+  var input   = document.getElementById("input");
 
-  addToList.addEventListener("submit", function (event) {
+  // add an event listener function on the 'click' of the submit button
+  submit.addEventListener("click", function (event) {
     event.preventDefault();
-    var addItem   = document.getElementById('addItem').value;
-    var entry     = document.createElement('li');
-    entry.appendChild(document.createTextNode(addItem));
-    list.appendChild(entry);
+
+    // upon the click of the submit event listener... a new item is created.
+    // Assign to NEW variable which equals document.createElement('li'), since it becomes a list item.
+    var newItem = document.createElement('li');
+    newItem.setAttribute('class', 'single-item');
+    newItem.setAttribute('id', 'item');
+    // .setAttribute on the new variable allows you to set class and id for yet to made items
+
+    // Submit event listener also creates div for checkbox. Can assign the className
+    // with .className on the new variable assigned to create the div.
+    // Append the new div to the newItem variable with appendChild
+    var checkboxDiv = document.createElement("div");
+    checkboxDiv.className = "checkbox";
+    checkboxDiv.innerHTML = "[ ]";
+    newItem.appendChild(checkboxDiv);
+
+    // Create another variable for the div to hold the input value.
+    // Add the input.value by calling the variable.innerHTML equal to the input.value
+    // Then append that new variable (itemNameDiv) to the newItem variable from above
+    var itemNameDiv = document.createElement("div");
+    itemNameDiv.className = "item-name";
+    itemNameDiv.innerHTML = input.value;
+    newItem.appendChild(itemNameDiv);
+
+    var deleteDiv       = document.createElement("div");
+    deleteDiv.className = "delete";
+    deleteDiv.innerHTML = "X";
+    newItem.appendChild(deleteDiv);
+
+    // event listener for checkbox to mark as complete on a click function
+    checkboxDiv.addEventListener("click", function() {
+      // use 'this.'
+    });
+
+    // event listener to turn item grey upon click
+    itemNameDiv.addEventListener("click", function () {
+      this.style.color="grey";
+    });
+
+    // event listener on checkbox to mark as complete on click.  This should lead to another confirm delete button.
+    // Confirm delete button will remove li.
+    deleteDiv.addEventListener("click", function() {
+      this.parentNode.parentNode.removeChild(this.parentNode);
+    });
+
+    // Appends the Whole new item created to the list variable created at top
+    list.appendChild(newItem);
+
+    // clears input to blank
+    input.value = "";
   });
 };
-
-
-// attempt 3 ----------------- name not appearing ----------------------------
-// window.onload = function() {
-//   var input = document.getElementById('input');
-//   var btn = document.getElementById('btn');
-//   var lists = document.getElementById('todo');
-//
-//   var makeTaskHtml = function(str) {
-//     var el = document.createElement('li');
-//     return el;
-//   };
-//
-//   var addTask = function(task) {
-//     lists.appendChild(task);
-//   };
-//
-//   var onInput = function() {
-//     var str   = input.value;
-//       addTask(makeTaskHtml(str));
-//   };
-//
-//   btn.addEventListener('click', onInput);
-//     input.focus();
-// };
-
-// attempt 2 ------------------------------------------
-// window.onload = function () {
-//   var addToList = document.getElementById("addToList");
-//
-//   addToList.addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     var addItem   = addToList.elements.addItem.value;
-//     var li = document.getElementById('addItem');
-//     var newText = document.createTextNode('blue');
-//     li.appendChild(newText);
-//   });
-//
-// };
